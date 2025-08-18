7290024991e68cc76f18ded6f63e4634a81813d2
Hossein <hosseinzahmatkesh39@gmail.com>
2025-08-18 08:20:13 +0330

    first commit

---
+++ public/main.js
@@ -0,0 +1,31 @@
+const socket = io()
+const clientsTotal = document.getElementById('clients-total')
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+messageForm.addEventListener('submit', (e) => {
+    //safhe refresh nshe
+    e.preventDefault()
+    sendMessage()
+})
+socket.on('client-total', (data) => {
+
+    console.log('data')
+    clientsTotal.innerText = `total clients : ${data}`
+
+})
+
+function sendMessage(){
+    const data = {
+        name: nameInput.value,
+        message: messageInput.value,
+        dataTime: new Date()
+    }
+    socket.emit('message', data)
+}
+
+socket.on('chat-message', (data) => {
+    console.log(data.message)
+})
